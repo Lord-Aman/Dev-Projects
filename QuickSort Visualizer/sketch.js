953797b397803e244497c510b974cf28d00db8d2
Aman <aakashaman1770@gmail.com>
2021-06-06 15:49:20 +0000

    initial commit

--- QuickSort Visualizer/sketch.js
+++ QuickSort Visualizer/sketch.js
@@ -4,7 +4,7 @@ let i = 0;
 let w = 10;
 
 function setup() {
-    createCanvas(800, 200);
+    createCanvas(fullScreen());
     values = new Array(floor(width / w));
     for (let i = 0; i < values.length; i++) {
         values[i] = random(height);
