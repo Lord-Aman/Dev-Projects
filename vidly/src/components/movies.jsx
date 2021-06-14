b323279c8d2d5a879c3b2822c5a2f3e6ea2626be
Aman <aakashaman1770@gmail.com>
2021-06-14 09:39:23 +0000

    initital commit

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./movies.css";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -6,17 +7,41 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
-  handleDelete = (movie) => {};
+  handleDelete = (movie) => {
+    console.log(movie);
+  };
 
   render() {
     return (
-      <div>
-        <p>
-          <strong>
-            Showing {this.state.movies.length} movies in the database.
-          </strong>
-        </p>
-      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate </th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td key={movie.genre._id}>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
