1b458f041328cebc3b3a6cd0ccf640c8aeebb91a
Aman <aakashaman1770@gmail.com>
2021-06-19 22:34:17 +0000

    sample build for CSB

---
+++ hosting-classic-gatsby/src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Home',
+    offset: -30,
+  },
+  {
+    path: 'features',
+    label: 'Features',
+    offset: 0,
+  },
+  {
+    path: 'services',
+    label: 'Services',
+    offset: -40,
+  },
+  {
+    path: 'support',
+    label: 'Support',
+    offset: -70,
+  },
+];
