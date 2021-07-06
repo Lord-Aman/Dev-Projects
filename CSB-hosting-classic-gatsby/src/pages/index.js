3f079e0d5436a123b1fc18727c12532889b5c8a1
Aman <aakashaman1770@gmail.com>
2021-07-06 20:09:14 +0000

    initial commit

--- CSB-hosting-classic-gatsby/src/pages/index.js
+++ CSB-hosting-classic-gatsby/src/pages/index.js
@@ -11,21 +11,21 @@ import "typeface-bree-serif";
 import "typeface-dm-sans";
 import Clients from "../sections/OurClients/Clients";
 import Testimonials from "../sections/Testimonials/testimonials";
-import BackgroundAnimation from "components/Animations/backgroundAnimation"
-import Counter from "sections/Counter/Counter"
+import BackgroundAnimation from "components/Animations/backgroundAnimation";
+import Counter from "sections/Counter/Counter";
 
 export default function IndexPage() {
   return (
     <StickyProvider>
-        <BackgroundAnimation/>
+      <BackgroundAnimation />
       <Layout>
         <Carousel />
         {/* How We Work */}
         <Features />
         {/* How We Work Ends Here  */}
         <Services />
-        <Counter/>
         <Clients />
+        <Counter />
         <Testimonials />
         <Blogs />
       </Layout>
